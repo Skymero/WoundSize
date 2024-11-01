b2fc4068aabf7e4160bd18871789563adcc2a1cd
Skymero <rmarti0516@gmail.com>
2024-11-01 07:50:57 -0700

    editing wound analysis code for appwrite function use

--- wound_analysis.py
+++ wound_analysis.py
@@ -52,23 +52,71 @@ def calculate_distance(time_of_flight):
 
 def getArea(radius):
     #radius * 2 = objSize
-    # 158px = 0.9in
-    # 79px = 0.45in
-    # px = 0.00569620253164556962025316455696 in
+    # 158px = 0.9in -> the full width|diameter of the quarter is 158 pixels in length
+    # 175.556 px  = 1 in
+    # 79px = 0.45in -> quarter's radius
+    # pixel size lenght = px width = 1/175.556
+    # area per pixel = (1/175.556)^2
+    
+    # px = 0.00569620253164556962025316455696 in 
     # px^2 = 3.2446723281525396571062329754847 * pow(10,-5) in^2
     
-    # px^2 * pixelCount = pixelArea
-    # in^2 * pixelCount = pixelArea
+    # px^2 * pixelCount = pixelArea_Px
+    # in^2 * pixelCount = pixelArea_In
+    
+    #1 get the size of a pixel - the radius of the object is hardcoded for now
+    # TODO: add logic to detect quarter and calculate pixel area per pixel from that
+    # we're assuming each wound is at the same distance every time
+    
+    #objectSize = size of a quarter on inches
+
+    """
+    Calculate the area of a given wound image in square inches.
+
+    The area calculation is based on the assumption that the size of the quarter
+    is 0.9 inches in diameter and 158 pixels in length. The area per pixel is
+    calculated as (1/175.556)^2 square inches. The total area is the product of
+    the area per pixel and the total number of pixels in the wound image.
+
+    Returns
+    -------
+    woundArea : float
+        The calculated area of the wound in square inches.
+    """
 
     objectSize = 0.9
-    pixelPerInch = (objectSize /2) / radius
-    areaPerPixel = pixelPerInch * pixelPerInch
-    pixelCount = getPixelCount()
-    woundArea = areaPerPixel * pixelCount 
+    
+    pixelPerInch = 158/objectSize # how many pixels equal an inch | pixel/inch
+    
+    #areaPerPixel = pixelPerInch * pixelPerInch # (px/inch)^2
+    areaPerPixel = 3.2446723281525396571062329754847 * pow(10,-5) # (px/inch)^2
+    
+    
+    pixelCount = getPixelCount() # total number of pixels
+    
+    woundArea = areaPerPixel * pixelCount # (px/inch)^2 * total number of pixels(scalar) -> 
     
     return woundArea
 
 def getPixelCount():
+
+    """
+    Count the total number of pixels in the wound mask image.
+
+    Opens the file "wound_mask.txt" and reads it line by line. For each line, it
+    splits the line into individual elements and checks if the element is '255'
+    or starts with a number greater than '0'. If the element meets one of these
+    conditions, it increments the totalPixelCount by 1.
+
+    After processing all the lines in the file, it prints the totalPixelCount
+    and returns it.
+
+    Returns
+    -------
+    int
+        The total number of pixels in the wound mask image.
+    """
+
     totalPixelCount = 0
     # rowCount = 0
     # colCount = 0
@@ -95,133 +143,98 @@ def getPixelCount():
 
     return totalPixelCount
 
-def main(filepath):
+def main(image):
 
-    # files = glob('C:\\Users\\MartinezR\\AI_Scripts\\woundData\\*.png')
-    files = glob(filepath)
-    pwatArr =[]
-    woundSizeArr = []
+    lbl = "wound"
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(21, 21))        
+    
+    # load the image in RGB fmt
+    img = cv2.imread(image)[..., ::-1]
+    
+    if img is None:
+        print("Failed to load image")
+        return None
+        
+    # get the semantic segmentation mask
+    mask = wound_segmentation(
+    img=img,
+    tol=0.5,
+    verbose=False,
+    )
 
-    if not files:
-        print("No files found.")
+    if mask is None:
+        print(f"Failed to get segmentation mask for image")
+        return None
+
+    # un-pack the semantic mask
+    wound_mask, body_mask, bg_mask = cv2.split(mask)
+    
+    # compute the wound PWAT
+    pwat = evaluate_PWAT_score(
+    img=img,
+    mask=mask,
+    verbose=False,
+    )
+
+
+    if pwat is None:
+        print(f"Failed to evaluate PWAT score for image")
+        return None
+
+    # mask the image according to the wound
+    wound_masked = cv2.bitwise_and(
+        img, 
+        img, 
+        mask=wound_mask
+    )
+
+
+    if wound_mask is None:
+        print('wound_mask is None')
     else:
-        print(f"Files found: {files}")
+        print('wound_mask is OK')
+        # print(body_mask)
+        # 
+        with open('wound_mask.txt', 'w') as fileB:
+            for row in wound_mask:
+                fileB.write(' '.join(map(str, row)) + '\n')
 
-    label = ['wound_A', 'wound_B', 'cound_C', 'wound_D', 'wound_E', 'wound_F', 'wound_G', 'wound_H', 'wound_I']
+    #################################################################
+    
+    r = 79
+    #################################################################
+    wound_area = getArea(r) 
+    print("Wound area: ", wound_area)
 
-    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(21, 21))
-
-    for f, ax, lbl in zip(files, axes.ravel(), label):
-        
-        img = cv2.imread(f)[..., ::-1]
-        
-        if img is None:
-            print(f"Failed to load image: {f}")
-            continue
-        
-        # # load the image in RGB fmt
-        # img = cv2.imread(f)[..., ::-1]
-        
-        # get the semantic segmentation mask
-        mask = wound_segmentation(
-        img=img,
-        tol=0.5,
-        verbose=False,
-        )
-
-        if mask is None:
-            print(f"Failed to get segmentation mask for image: {f}")
-            continue
-
-        # un-pack the semantic mask
-        wound_mask, body_mask, bg_mask = cv2.split(mask)
-        
-        # compute the wound PWAT
-        pwat = evaluate_PWAT_score(
-        img=img,
-        mask=mask,
-        verbose=False,
-        )
-
-
-        if pwat is None:
-            print(f"Failed to evaluate PWAT score for image: {f}")
-            continue
-
-        # mask the image according to the wound
-        wound_masked = cv2.bitwise_and(
-            img, 
-            img, 
-            mask=wound_mask
-        )
-
-
-        # if body_mask is None:
-        #     print('body_mask is None')
-        # else:
-        #     print('body_mask is OK')
-        #     # print(body_mask)
-        #     with open('body_mask.txt', 'w') as fileB:
-        #         for row in body_mask:
-        #             fileB.write(' '.join(map(str, row)) + '\n')
-
-
-        if wound_mask is None:
-            print('wound_mask is None')
-        else:
-            print('wound_mask is OK')
-            # print(body_mask)
-            with open('wound_mask.txt', 'w') as fileB:
-                for row in wound_mask:
-                    fileB.write(' '.join(map(str, row)) + '\n')
-
-        ################################################################
-        # print(type(f))
-        # # image_path = f.read()
-        # # print(type(f))
-        # x, y, r = CoinDetection.CoinDetection(f)
-        # print("x, y, r: ", x, y, r)
-        r = 79
-        
-        # circle = plt.Circle((x, y), r, color='r', fill=False)
-
-        #pixelArea, pxlPerMetric = getPixerPerMetric(r) #(in^2 / px , px/in)
-
-        #################################################################
-        wound_area = getArea(r) 
-        print("Wound area", lbl,": ", wound_area)
-        woundSizeArr.append(wound_area)
-        pwatArr.append(pwat)
-
-        # display the result
-        ax.imshow(wound_masked)
-        ax.imshow(img, alpha=0.75)
-        ax.contour(wound_mask, colors='lime', linewidths=1)
-        # plt.savefig(f'{lbl}.png')
-        ax.grid()
-        
-        # ax.add_patch(circle)
-        ax.text(0, 0, lbl, fontsize=8, color='k', weight='bold')
-        t1 = ax.text(0.3, 0.2, f"Wound Area:{wound_area:.3f}", 
+    # display the result
+    ax.imshow(wound_masked)
+    ax.imshow(img, alpha=0.75)
+    ax.contour(wound_mask, colors='lime', linewidths=1)
+    # plt.savefig(f'{lbl}.png')
+    ax.grid()
+    
+    # ax.add_patch(circle)
+    ax.text(0, 0, lbl, fontsize=8, color='k', weight='bold')
+    t1 = ax.text(0.3, 0.2, f"Wound Area:{wound_area:.3f}", 
+            transform=ax.transAxes, fontsize=8)
+    t = ax.text(0.3, 0.05, f"Deepskin's score: {pwat:.3f}",
                 transform=ax.transAxes, fontsize=8)
-        t = ax.text(0.3, 0.05, f"Deepskin's score: {pwat:.3f}",
-                    transform=ax.transAxes, fontsize=8)
-        t.set_bbox(dict(facecolor='white', alpha=0.75, edgecolor='k'))
-        t1.set_bbox(dict(facecolor='white', alpha=0.75, edgecolor='k'))
-        ax.axis('off')
+    t.set_bbox(dict(facecolor='white', alpha=0.75, edgecolor='k'))
+    t1.set_bbox(dict(facecolor='white', alpha=0.75, edgecolor='k'))
+    ax.axis('off')
+
 
     
-       
     fig.tight_layout()
     plt.savefig('wound_table.png') 
     returnImage =cv2.imread('wound_table.png')
     returnImage = cv2.resize(returnImage, (800, 600)) 
     # plt.show()
 
-    return pwatArr, woundSizeArr, returnImage
+    return 
 
 if __name__ == "__main__":
-    filepath = input("Please enter the filepath: ")
+    filepath = "wound.png"
     p, w, rimg = main(filepath)
     print("Deepskin's score: ", p)
     print("Wound Area: ", w)
